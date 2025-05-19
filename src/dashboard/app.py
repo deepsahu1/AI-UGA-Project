1fd675ea6058266d624e65dc99ba2c747eebd5fc
deepsahu1 <ds42514@uga.edu>
2025-05-19 09:46:48 -0400

    dashboard integration with CSV

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -1,8 +1,12 @@
 import dash
 from dash import html, dcc
 from flask import Flask, request, jsonify
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
+import pandas as pd
 
+df = pd.read_csv("data/raw/data.csv")
+
+df.columns = df.columns.str.strip()
 
 #Flask server
 server = Flask(__name__)
@@ -15,17 +19,34 @@ app = dash.Dash(__name__, server = server)
 app.layout = html.Div([
     html.H1("Live Sensor Dashboard", style = {'fontSize' : '40px', 'textAlign' : 'center'}), 
     html.Div(id="live-reading", style={"fontSize": "24px", "marginTop": "20px"}),
-    dcc.Interval(id="interval-update", interval=3000, n_intervals=0) #Updates latest data every 3 seconds
+    dcc.Store(id="row-index", storage_type = "session", data=0),
+    dcc.Interval(id="interval-update", interval=5000, n_intervals=0) #Updates latest data every 15 minutes
 ])
 
 @app.callback(
         Output("live-reading", "children"),
-        Input("interval-update", "n_intervals")
+        Output("row-index", "data"),
+        Input("interval-update", "n_intervals"),
+        State("row-index", "data")
+        
 )
-def update(n):
-    return f"Latest Data: {server.config['latest_data']}"
-
-
+def update_dashboard(n, row_index):
+   if row_index is None:
+        row_index = 0
+   if row_index >= len(df):
+       return html.Div("Reached the end of the dataset."), row_index
+   current_row = df.iloc[row_index]
+   row_index += 1
+   result = html.Div([
+       html.P(f"Date: {current_row['Quarter_DDMMYYYY']}"),
+       html.P(f"Temp: {current_row['Temp']}°C"),
+       html.P(f"pH: {current_row['pH']}"),
+       html.P(f"Turbidity: {current_row['Turbidity']}"),
+       html.P(f"DO: {current_row['DO']} mg/L"),
+   ])
+   return result, row_index
+   
+"""
 @server.route("/post-sensor-data", methods = ["POST"])
 def post_sensor_data():
     latest_data = request.get_json() #gets JSON from request
@@ -33,7 +54,8 @@ def post_sensor_data():
 
     print("POST data successfully recieved", latest_data)
     return jsonify({"status": "received", "data": latest_data}), 200
+"""
 
 if __name__ == "__main__":
-    app.run(debug=False, port=8051)
+    app.run(debug=True, port=8049)
 
