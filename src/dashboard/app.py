4f24cd3382987beb963c95b7f7efa619bd99dfa7
deepsahu1 <ds42514@uga.edu>
2025-05-20 01:03:28 -0400

    better style on dashboard

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -12,16 +12,38 @@ df.columns = df.columns.str.strip()
 server = Flask(__name__)
 server.config["latest_data"] = "No data yet"
 
-#Flash app
+#Dash app
 app = dash.Dash(__name__, server = server)
+
+# Custom theme
+theme = {
+    'backgroundColor': '#1c1c1c',
+    'textColor': '#FFA500',
+    'fontFamily': 'Segoe UI, sans-serif',
+    'fontWeight': '500',  # Semi-bold
+}
 
 #Setting layout
 app.layout = html.Div([
-    html.H1("Live Sensor Dashboard", style = {'fontSize' : '40px', 'textAlign' : 'center'}), 
-    html.Div(id="live-reading", style={"fontSize": "24px", "marginTop": "20px"}),
+    html.Link(href="https://fonts.googleapis.com/css2?family=Segoe+UI&display=swap", rel="stylesheet"), # text font
+
+     html.H1("Live Sensor Dashboard", style={
+        'fontSize': '40px',
+        'textAlign': 'center',
+        'color': theme['textColor'],
+        'fontFamily': theme['fontFamily'],
+        'fontWeight': theme['fontWeight']
+    }), 
+     html.Div(id="live-reading", style={
+        "fontSize": "24px",
+        "marginTop": "20px",
+        "color": theme['textColor'],
+        "fontFamily": theme['fontFamily'],
+        "fontWeight": theme['fontWeight']
+    }),
     dcc.Store(id="row-index", storage_type = "session", data=0),
     dcc.Interval(id="interval-update", interval=5000, n_intervals=0) #Updates latest data every 15 minutes
-])
+], style={'backgroundColor': theme['backgroundColor'], 'height': '100vh', 'padding': '40px'})
 
 @app.callback(
         Output("live-reading", "children"),
@@ -37,14 +59,17 @@ def update_dashboard(n, row_index):
        return html.Div("Reached the end of the dataset."), row_index
    current_row = df.iloc[row_index]
    row_index += 1
-   result = html.Div([
+   return html.Div([
        html.P(f"Date: {current_row['Quarter_DDMMYYYY']}"),
        html.P(f"Temp: {current_row['Temp']}°C"),
        html.P(f"pH: {current_row['pH']}"),
        html.P(f"Turbidity: {current_row['Turbidity']}"),
        html.P(f"DO: {current_row['DO']} mg/L"),
-   ])
-   return result, row_index
+    ], style={
+       'color': theme['textColor'],
+       'fontFamily': theme['fontFamily'],
+       'fontWeight': theme['fontWeight']
+    }), row_index
    
 """
 @server.route("/post-sensor-data", methods = ["POST"])
